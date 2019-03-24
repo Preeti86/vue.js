bea791806a7cf8166f7b7eb618f87694024c1a70
preeti86 <preetijav@gmail.com>
2019-03-24 23:58:47 +0100

    update2

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+console.log('hello');
+
 new Vue({
     el:'#vue-app',
     data:{
-        name:'Preeti'
+        age:25
+    },
+    methods:{
+        add:function () {
+            this.age++;
+
+        },
+        subract:function(){
+            this.age--;
+        }
+
+
     }
 });
